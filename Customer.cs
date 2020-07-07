765ec960cfd8ed982fc20e6cf180b15a925eace1
Rgonzalez717 <61915917+Rgonzalez717@users.noreply.github.com>
2020-07-07 11:19:50 +0700

    classs Customer

---
+++ Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace responsi
+{
+    class Customer
+    {
+        public string Nama { get; set; }
+        public string Kode { get; set; }
+        public int Piutang { get; set; }
+    }
+}
